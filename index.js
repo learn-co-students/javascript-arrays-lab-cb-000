6a77c06f47cbce8ed0536caa501da1854c0d8818
Brian Navaluna <just.brian.en@mail.com>
2017-03-29 20:12:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeLastKitten(){
+  var new_kittens = kittens.slice(0,kittens.length-1);
+  return new_kittens;
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
