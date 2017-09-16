4a9d4fbc183ac9533985aacc7fc03232c454359e
Jordan Johnson <jkj86@hotmail.com>
2017-09-16 23:32:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var array = kittens.slice();
+  array.push(name);
+  return array;
+}
+
+function prependKitten(name){
+  var array = kittens.slice();
+  array.unshift(name);
+  return array;
+}
+
+function removeLastKitten(){
+  var array = kittens.slice();
+  array.pop();
+  return array;
+}
+
+function removeFirstKitten(){
+  var array = kittens.slice();
+  array.shift();
+  return array;
+}
