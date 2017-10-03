b36494ea001164f4c1e48c56622783ab7f725f9b
Gerald Ingraham Ingraham <gingraham@icloud.com>
2017-10-03 23:27:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+   var new_arr = [...kittens, name]
+   return new_arr
+}
+
+function prependKitten(name){
+  var new_arr = [name, ...kittens]
+  return new_arr
+}
+
+function removeLastKitten(){
+  var new_arr = kittens.slice(0, kittens.length -1);
+  return new_arr
+}
+
+function removeFirstKitten(){
+  var new_arr = kittens.slice(1);
+  return new_arr
+}
