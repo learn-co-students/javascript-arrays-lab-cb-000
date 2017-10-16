2a4c6029e9def3717169e0f5340a6cdc34a72c08
Cesar Castillo <cesarcast1llo@yahoo.com>
2017-10-16 22:29:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.unshift;
+  kittens.push(name);
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+kittens.unshift(name);
+return kittens
+}
+function destructivelyRemoveLastKitten() {
+kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+function appendKitten(name) {
+  var kitties = [...kittens, name];
+   return kitties
+}
+function prependKitten(name) {
+var kitties = [name, ...kittens]
+return kitties
+}
+function removeLastKitten() {
+  var kitties = kittens.slice(0, kittens.length - 1);
+   return kitties
+}
+function removeFirstKitten() {
+var kitties = kittens.slice(1)
+return kitties
+}
