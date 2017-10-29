16fc1819ccca544e6cab0874e5b15bf601698fdd
cptbrkfst <dyl@resonategroup.ca>
2017-10-29 18:28:26 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var destructivelyAppendKitten = name => kittens.push(name);
+
+var destructivelyPrependKitten = name => kittens.unshift(name);
+
+var destructivelyRemoveLastKitten = () => kittens.pop();
+
+var destructivelyRemoveFirstKitten = () => kittens.shift();
+
+var appendKitten = name => {
+  var newKittens = [...kittens, name];
+  return newKittens;
+};
+
+var prependKitten = name => {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+};
+
+var removeLastKitten = () => {
+  var newKittens = kittens.slice(0, -1);
+  return newKittens;
+};
+
+var removeFirstKitten = () => {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+};
