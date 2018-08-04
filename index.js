fbc1f9fb6d3f1965b36feeac8ea542b8203fcf10
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 04:34:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+
+var destructivelyAppendKitten = name => {
+  return kittens.push(name)
+}
+
+var destructivelyPrependKitten = name => {
+  return kittens.unshift(name)
+}
+
+var destructivelyRemoveLastKitten = () => {
+  return kittens.pop()
+}
+
+var destructivelyRemoveFirstKitten = () => {
+  return kittens.shift()
+}
+
+var appendKitten = name => {
+  return kittens.concat(name)
+}
+
+var appendKitten = name => {
+  return kittens.concat(name)
+}
+
+var prependKitten = name => {
+  var array = [name]
+  return array.concat(kittens)
+}
+
+var removeLastKitten = () => {
+  return kittens.slice(0, -1)
+
+}
+
+var removeFirstKitten = () => {
+  return kittens.slice(1)
+
+}
